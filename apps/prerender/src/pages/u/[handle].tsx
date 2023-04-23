8d8418e7e45e5ffee0abe8640936d1410303f728
sarthak2721 <131484008+sarthak2721@users.noreply.github.com>
2023-04-23 12:46:36 +0530

    Add virtualized list support for feed

---
+++ apps/prerender/src/pages/u/[handle].tsx
@@ -0,0 +1,53 @@
+import Profile from '@components/Profile';
+import { HANDLE_SUFFIX, LENSPROTOCOL_HANDLE } from 'data/constants';
+import { CustomFiltersTypes, PrerenderProfileDocument, ProfileFeedDocument } from 'lens';
+import { nodeClient } from 'lens/apollo';
+import type { GetServerSidePropsContext } from 'next';
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const handle = context.params?.handle;
+
+  // Cache the response for 60 days
+  context.res.setHeader('Cache-Control', 'public, max-age=5184000');
+
+  if (!handle) {
+    return {
+      props: { data: null }
+    };
+  }
+
+  let processedHandle;
+  if (handle.includes(HANDLE_SUFFIX)) {
+    processedHandle = handle;
+  } else {
+    processedHandle = handle === LENSPROTOCOL_HANDLE ? handle : handle.concat(HANDLE_SUFFIX);
+  }
+  const { data: profileData } = await nodeClient.query({
+    query: PrerenderProfileDocument,
+    variables: { request: { handle: processedHandle } }
+  });
+
+  if (profileData.profile) {
+    const profileId = profileData.profile.id;
+    const reactionRequest = { profileId };
+
+    const { data: profilePublicationsData } = await nodeClient.query({
+      query: ProfileFeedDocument,
+      variables: {
+        request: { profileId, customFilters: [CustomFiltersTypes.Gardeners], limit: 50 },
+        reactionRequest,
+        profileId
+      }
+    });
+
+    return {
+      props: { profile: profileData.profile, publications: profilePublicationsData.publications?.items }
+    };
+  }
+
+  return {
+    props: { profile: null, publications: null }
+  };
+}
+
+export default Profile;
